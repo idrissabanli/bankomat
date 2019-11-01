8f95bda3c5f7a44756f2bec6fe8ff515d99bedbf
idris <idris@labrin.net>
2019-11-01 13:41:42 +0400

    admin

--- bankomat/admin.py
+++ bankomat/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Cart
 
-admin.site.register(Cart)
+
+class CartAdmin(admin.ModelAdmin):
+    model = Cart
+    list_display = ('full_name', 'cart_number', 'password', 'balance',)
+
+
+admin.site.register(Cart, CartAdmin)
